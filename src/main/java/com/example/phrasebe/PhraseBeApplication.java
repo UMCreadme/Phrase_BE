619f957b71a2d093109942ada571d9a7ce1c91b6
OSSU <o.ss.u2056@gmail.com>
2024-12-18 17:16:28 +0900

    :tada: 프로젝트 초기 세팅

---
+++ src/main/java/com/example/phrasebe/PhraseBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.phrasebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhraseBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhraseBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/phrasebe/PhraseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phrasebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhraseBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
